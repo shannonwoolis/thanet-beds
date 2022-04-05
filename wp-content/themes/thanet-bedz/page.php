9fe164ec1373cbd97781ceb72454f821961ba6ed
shannon.woolis <shannon.woolis@adtrak.co.uk>
2022-04-05 22:28:01 +0100

    add: rest of main pages

--- wp-content/themes/thanet-bedz/page.php
+++ wp-content/themes/thanet-bedz/page.php
@@ -6,4 +6,14 @@ $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 
+if(is_page('reviews')) {
+    // Reviews
+    $reviews = array(
+        'post_type'           => 'reviews',
+        'post_status'         => 'publish',
+        'posts_per_page'      => -1,
+    );
+    $context['reviewsAll'] = new Timber\PostQuery($reviews);
+}
+
 Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page.twig' ], $context );
