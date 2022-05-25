ffa3361ae43e3d5c8fed562b4c5cbe12260cb0bc
shannon.woolis <shannon.woolis@adtrak.co.uk>
2022-05-25 21:43:46 +0100

    fix: site speed

--- wp-content/themes/thanet-bedz/_functions/Scripts.php
+++ wp-content/themes/thanet-bedz/_functions/Scripts.php
@@ -112,3 +112,19 @@ class Scripts
     {
     }
 }
+
+/**
+ * Disable WooCommerce block styles (front-end).
+ */
+function themesharbor_disable_woocommerce_block_styles() {
+    wp_dequeue_style( 'wc-blocks-style' );
+}
+add_action( 'wp_enqueue_scripts', 'themesharbor_disable_woocommerce_block_styles' );
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
